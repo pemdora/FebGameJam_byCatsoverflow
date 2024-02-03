a80545d32440ef563235ef36c38558494fbd7b04
pLeet <158078828+pLeetDev@users.noreply.github.com>
2024-02-03 23:13:31 +0100

    Minor change

--- Assets/Game/Scripts/Audio/AudioManager.cs
+++ Assets/Game/Scripts/Audio/AudioManager.cs
@@ -5,18 +5,27 @@ public class AudioManager : MonoBehaviour
 {
     private AudioSource _audiosource;
 
-    private void Awake() => _audiosource = GetComponent<AudioSource>();
+    private void Awake()
+    {
+        _audiosource = GetComponent<AudioSource>();
+    }
 
     /// <summary>
     /// Plays an audio file at max volume.
     /// </summary>
     /// <param name="clip">The AudioClip to play.</param>
-    public void PlaySound(AudioClip clip) => _audiosource?.PlayOneShot(clip, 1f);
+    public void PlaySound(AudioClip clip)
+    {
+        _audiosource?.PlayOneShot(clip, 1f);
+    }
 
     /// <summary>
     /// Plays an audio file with a set volume.
     /// </summary>
     /// <param name="clip">The AudioClip to play.</param>
     /// <param name="volume">The volume of the AudioClip to play.</param>
-    public void PlaySoundWithVolume(AudioClip clip, float volume) => _audiosource?.PlayOneShot(clip, volume);
+    public void PlaySoundWithVolume(AudioClip clip, float volume)
+    {
+        _audiosource?.PlayOneShot(clip, volume);
+    }
 }
