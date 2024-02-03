29656ea2247fb286b5729aa47fb44d7d0c076438
ShidyGames <shidygames@gmail.com>
2024-02-03 19:47:40 +0100

    Implemented a basic AudioManager.
    Added a sample scene to test out the audio manager, plus 2 audio samples.

---
+++ Assets/Game/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource _audiosource;
+
+    private void Awake() => _audiosource = GetComponent<AudioSource>();
+
+    /// <summary>
+    /// Plays an audio file.
+    /// </summary>
+    /// <param name="clip">The AudioClip to play.</param>
+    public void PlaySound(AudioClip clip) => _audiosource?.PlayOneShot(clip);
+
+    /// <summary>
+    /// Plays an audio file with a set volume.
+    /// </summary>
+    /// <param name="clip">The AudioClip to play.</param>
+    /// <param name="volume">The volume of the AudioClip to play.</param>
+    public void PlaySoundWithVolume(AudioClip clip, float volume) => _audiosource?.PlayOneShot(clip, volume);
+}
