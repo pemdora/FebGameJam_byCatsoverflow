610e3237fd26e33da1a224d1ba245c0bc446513e
rene-tom <re.tom2003@gmail.com>
2024-02-22 20:37:25 +0100

    Fill Animation

--- Assets/Game/Scripts/HUD/PercentageUI.cs
+++ Assets/Game/Scripts/HUD/PercentageUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,10 @@ public class PercentageUI : MonoBehaviour
     [SerializeField] private TMP_Text _percentageText;
     [SerializeField] private Slider _objectiveSlider;
     [SerializeField] private Image _filler;
+    [SerializeField] private float _animationDuration = 1.0f;
     private float _previousPercentage;
+    private float _animationPercentage;
+    private Coroutine _fillCoroutine;
 
     void Start()
     {
@@ -31,9 +35,20 @@ public class PercentageUI : MonoBehaviour
 
         if (_spaceshipManager.HasSpaceship && _spaceshipManager.Percentage <= 100)
         {
-            _previousPercentage = Mathf.FloorToInt(_spaceshipManager.Percentage);
-            _percentageText.text = _previousPercentage.ToString();
-            _filler.fillAmount = _spaceshipManager.Percentage / 100f;
+                int nextPercentage = Mathf.FloorToInt(_spaceshipManager.Percentage);
+            if (nextPercentage != _previousPercentage)
+            {
+                if (_fillCoroutine == null)
+                {
+                    _fillCoroutine = StartCoroutine(FillCoroutine(_previousPercentage,nextPercentage,_animationDuration));
+                }
+                else
+                {
+                    StopCoroutine(_fillCoroutine);
+                    _fillCoroutine = StartCoroutine(FillCoroutine(_animationPercentage, nextPercentage, _animationDuration));
+                }
+                _previousPercentage = nextPercentage;
+            }
         }
         else
         {
@@ -41,5 +56,25 @@ public class PercentageUI : MonoBehaviour
             _percentageText.text = "0";
             _filler.fillAmount = 0;
         }
+    }
+
+    private IEnumerator FillCoroutine(float previousValue, float NextValue, float duration)
+    {
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / duration;
+            _animationPercentage = Mathf.FloorToInt(Mathf.Lerp(previousValue, NextValue, percent));
+            Debug.Log("LerpVal = " + _animationPercentage.ToString() +", percent = " +percent);
+            _percentageText.text = _animationPercentage.ToString();
+            _filler.fillAmount = _animationPercentage / 100f;
+            yield return null;
+        }
+        EndFillCoroutine();
+    }
+
+    private void EndFillCoroutine()
+    {
+        _fillCoroutine = null;
     }
 }
