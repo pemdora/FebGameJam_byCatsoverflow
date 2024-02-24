073351c501351f905d01144824f5d6359cd91904
rene-tom <re.tom2003@gmail.com>
2024-02-24 22:17:13 +0100

    Added Manager's Emotions

--- Assets/Game/Scripts/Character/Manager/ManagerFaceAnimation.cs
+++ Assets/Game/Scripts/Character/Manager/ManagerFaceAnimation.cs
@@ -53,30 +53,94 @@ public class ManagerFaceAnimation : MonoBehaviour
     [ColorUsageAttribute(true, true)]
     [SerializeField] private Color _emissionColor = Color.white;
     // List of the emotions that can make the manager, associed to the mouth and eyes index
-    [SerializeField] private List<EmotionDictionaryEntry> _emotionDictionary= new List<EmotionDictionaryEntry>();
+    [SerializeField] private List<EmotionDictionaryEntry> _emotionDictionary = new List<EmotionDictionaryEntry>();
     [SerializeField] ManagerEmotions _currentEmotion = ManagerEmotions.Neutral;
+    [SerializeField] Vector3 _eyeOffset;
+    [SerializeField] Gradient _gradient;
 
-    // [Range(0f, 10f)]
-    // [SerializeField] private float _mouthAnimationSpeed = 1f; //polish
 
     [Header("Face Animations")]
     [SerializeField] private List<FaceAnimation> eyesAnimations = new List<FaceAnimation>();
     [SerializeField] private List<FaceAnimation> mouthAnimations = new List<FaceAnimation>();
- 
- 
+
+
     [Header("références")]
     [SerializeField] private DecalProjector eyesDecalProjector;
     [SerializeField] private DecalProjector mouthDecalProjector;
-    [SerializeField] private Material eyeDecalMaterial;    
-    [SerializeField] private Material mouthDecalMaterial; 
+    [SerializeField] private Material eyeDecalMaterial;
+    [SerializeField] private Material mouthDecalMaterial;
     [SerializeField] private GameObject face;
 
-  
-    public void Awake(){
+    [Header("Feelings Values")]
+    [Range(0, 10)]
+    [SerializeField] float _feelingBar;
+    [SerializeField] float _cargoValue = 3;
+    [SerializeField] float _dropValue = 2;
+    [SerializeField] float _placeValue = 2;
+    float _nextFeeling;
+    bool _shouldUpdate = false;
+    float _changeSpeed = 5f;
+
+
+
+    public void Awake()
+    {
         //selection of the first eyes and mouth
         changeEyes(eyesAnimations[0].name);
         changeMouth(mouthAnimations[0].name);
         changeSpriteColor(_color);
+    }
+
+    public void Start()
+    {
+        ScoreManager scoreManager = FindObjectOfType<ScoreManager>();
+        if (scoreManager)
+        {
+            scoreManager.OnCargoReachedMinimumRequirement.AddListener(CargoLeavingHandler);
+        }
+        PickManager pickManager = FindObjectOfType<PickManager>();
+        if(pickManager)
+        {
+            pickManager.OnDropWare.AddListener(WareDropHandler);
+            pickManager.OnPlaceWare.AddListener(WarePlaceHandler);
+        }
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        if(gameManager)
+        {
+            gameManager.OnGameOverEvent.AddListener(ResetManagerAnimations);
+        }
+        _nextFeeling = _feelingBar;
+    }
+
+    public void Update()
+    {
+
+        //for polish animation
+        //Camera camera = Camera.main;
+        //eyesDecalProjector.transform.LookAt(camera.transform.position);
+
+        //eyesDecalProjector orientation must always be align with the face center
+        eyesDecalProjector.transform.LookAt(face.transform.position);
+        mouthDecalProjector.transform.LookAt(face.transform.position);
+        ComputeCurrentEmotion();
+        ComputeColors();
+        ChangeEmotion(_currentEmotion);
+        changeSpriteColor(_color);
+        if (_shouldUpdate)
+        {
+            if (_nextFeeling == _feelingBar)
+            {
+                _shouldUpdate = false;
+            }
+            int sign = (_nextFeeling > _feelingBar) ? 1 : -1;
+            float time = Time.deltaTime;
+            _feelingBar += time * sign * _changeSpeed;
+            if (Mathf.Abs(_feelingBar - _nextFeeling) < 0.1f)
+            {
+                _feelingBar = _nextFeeling;
+            }
+        }
+
     }
 
     public void changeSpriteColor(Color color)
@@ -89,11 +153,11 @@ public class ManagerFaceAnimation : MonoBehaviour
     }
 
 
-   public void changeEyes(string eyeName)
+    public void changeEyes(string eyeName)
     {
-        foreach(FaceAnimation eyes in eyesAnimations)
+        foreach (FaceAnimation eyes in eyesAnimations)
         {
-            if(eyes.name == eyeName)
+            if (eyes.name == eyeName)
             {
                 eyeDecalMaterial.SetTexture(BASE_MAP, eyes.texture.texture);
                 eyesDecalProjector.material = eyeDecalMaterial;
@@ -102,32 +166,23 @@ public class ManagerFaceAnimation : MonoBehaviour
     }
 
     public void changeMouth(string mouthName)
-    {  
-        foreach(FaceAnimation mouth in mouthAnimations)
+    {
+        foreach (FaceAnimation mouth in mouthAnimations)
         {
-            if(mouth.name == mouthName)
-            {               
+            if (mouth.name == mouthName)
+            {
                 mouthDecalMaterial.SetTexture(BASE_MAP, mouth.texture.texture);
                 mouthDecalProjector.material = mouthDecalMaterial;
             }
-        }       
+        }
     }
 
-    public void Update(){
-
-        //for polish animation
-        //eyesDecalProjector orientation must always be align with the face center
-        eyesDecalProjector.transform.LookAt(face.transform.position);
-        mouthDecalProjector.transform.LookAt(face.transform.position);
-
-        changeSpriteColor(_color);
-        ChangeEmotion(_currentEmotion);
-
-    }
+    
+    
 
     public void ChangeEmotion(ManagerEmotions emotions)
     {
-        foreach(EmotionDictionaryEntry emotionEntry in _emotionDictionary)
+        foreach (EmotionDictionaryEntry emotionEntry in _emotionDictionary)
         {
             if (emotionEntry._emotion == emotions)
             {
@@ -138,4 +193,67 @@ public class ManagerFaceAnimation : MonoBehaviour
             }
         }
     }
+
+    void ComputeCurrentEmotion()
+    {
+        if (_feelingBar <= 2)
+        {
+            _currentEmotion = ManagerEmotions.Livid;
+        }
+        else if (_feelingBar < 4)
+        {
+            _currentEmotion = ManagerEmotions.Unhappy;
+        }
+        else if (_feelingBar < 6)
+        {
+            _currentEmotion = ManagerEmotions.Neutral;
+        }
+        else if (_feelingBar < 8)
+        {
+            _currentEmotion = ManagerEmotions.Pleased;
+        }
+        else
+        {
+            _currentEmotion = ManagerEmotions.Happy;
+        }
+    }
+
+    void ComputeColors()
+    {
+        float lerpIndex = _feelingBar  * 0.1f;
+        _emissionColor = _gradient.Evaluate(lerpIndex);
+    }
+
+    void AddFeeling(float feelingValue)
+    {
+        _nextFeeling += feelingValue;
+        //_feelingBar += feelingValue;
+        _shouldUpdate = true;
+    }
+
+    void ResetManagerAnimations()
+    {
+        _feelingBar = 5;
+        _nextFeeling = 5;
+        _shouldUpdate = false;
+        _currentEmotion = ManagerEmotions.Neutral;
+    }
+
+    void CargoLeavingHandler(bool isFilled)
+    {
+        float feelingValue = (isFilled) ? _cargoValue : -_cargoValue;
+        AddFeeling(feelingValue);
+    }
+
+    void WareDropHandler(WareEventData data)
+    {
+        AddFeeling(-_dropValue);
+    }
+
+    void WarePlaceHandler(WareEventData data)
+    {
+        AddFeeling(_placeValue);
+    }
+
+
 }
