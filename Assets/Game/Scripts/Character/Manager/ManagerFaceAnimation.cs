57716675d7cee0f5cd2c585ebbe69a610e1ac633
rene-tom <re.tom2003@gmail.com>
2024-02-25 20:00:38 +0100

    Added Mouth animation when angry

--- Assets/Game/Scripts/Character/Manager/ManagerFaceAnimation.cs
+++ Assets/Game/Scripts/Character/Manager/ManagerFaceAnimation.cs
@@ -5,6 +5,7 @@ using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering.LookDev;
 using UnityEngine.Rendering.Universal;
+using static UnityEngine.Rendering.DebugUI;
 
 
 [System.Serializable]
@@ -75,7 +76,8 @@ public class ManagerFaceAnimation : MonoBehaviour
     [Header("Feelings Values")]
     [Range(0, 10)]
     [SerializeField] float _feelingBar;
-    [SerializeField] float _cargoValue = 3;
+    [SerializeField] float _cargoCompleteValue = 3;
+    [SerializeField] float _cargoEmptyValue = 3;
     [SerializeField] float _dropValue = 2;
     [SerializeField] float _placeValue = 2;
     float _nextFeeling;
@@ -83,6 +85,7 @@ public class ManagerFaceAnimation : MonoBehaviour
     float _changeSpeed = 5f;
 
     Coroutine _currentCoroutine;
+    Coroutine _mouthCoroutine;
 
 
 
@@ -117,11 +120,6 @@ public class ManagerFaceAnimation : MonoBehaviour
 
     public void Update()
     {
-
-        //for polish animation
-        //Camera camera = Camera.main;
-        //eyesDecalProjector.transform.LookAt(camera.transform.position);
-
         //eyesDecalProjector orientation must always be align with the face center
         eyesDecalProjector.transform.LookAt(face.transform.position);
         mouthDecalProjector.transform.LookAt(face.transform.position);
@@ -183,6 +181,7 @@ public class ManagerFaceAnimation : MonoBehaviour
 
     public void ChangeEmotion(ManagerEmotions emotions)
     {
+        if (_mouthCoroutine != null) return;
         foreach (EmotionDictionaryEntry emotionEntry in _emotionDictionary)
         {
             if (emotionEntry._emotion == emotions)
@@ -195,19 +194,13 @@ public class ManagerFaceAnimation : MonoBehaviour
         }
     }
 
-    IEnumerator NodCoroutine(bool approval, float feelingValue)
+    IEnumerator NodCoroutine(float feelingValue)
     {
         Quaternion baseRotation = transform.rotation;
         float totalTime = 0.5f;
         int nodNumber = 1;
-        if (approval)
-        {
-            nodNumber += +Mathf.Max(Mathf.FloorToInt(feelingValue / 4 - 1), 0);
-        }
-        else
-        {
-            nodNumber += +Mathf.Max(Mathf.FloorToInt(feelingValue / 4 - 1), 0);
-        }
+        float differenceHalf = feelingValue - 5;
+        nodNumber += +Mathf.Max(Mathf.FloorToInt(differenceHalf / 3), 0);
 
         float nodTime = totalTime / nodNumber;
         for (int i = 0; i < nodNumber; i++)
@@ -216,15 +209,7 @@ public class ManagerFaceAnimation : MonoBehaviour
             while (percent < 1)
             {
                 percent += Time.deltaTime / nodTime;
-                Quaternion movement;
-                if (approval)
-                {
-                    movement = (Quaternion.Euler(50 * Mathf.Sin(percent * Mathf.PI), 0, 0));
-                }
-                else
-                {
-                    movement = (Quaternion.Euler(0, 30 * Mathf.Sin(percent * Mathf.PI), 0));
-                }
+                Quaternion movement = (Quaternion.Euler(50 * Mathf.Sin(percent * Mathf.PI), 0, 0));
                 transform.rotation = baseRotation * movement;
                 yield return null;
             }
@@ -233,6 +218,31 @@ public class ManagerFaceAnimation : MonoBehaviour
         _currentCoroutine = null;
     }
 
+    IEnumerator DenyCoroutine(float feelingValue)
+    {
+        Quaternion baseRotation = transform.rotation;
+        float totalTime = 0.25f;
+        int nodNumber = 1;
+
+        float differenceHalf = 5 - feelingValue;
+        nodNumber += +Mathf.Max(Mathf.FloorToInt(differenceHalf / 3), 0);
+
+        float nodTime = totalTime / nodNumber;
+        for (int i = 0; i < nodNumber; i++)
+        {
+            transform.rotation = baseRotation * Quaternion.Euler(0, -15, 0) ;
+            float percent = 0;
+            while (percent < 1)
+            {
+                percent += Time.deltaTime / nodTime;
+                Quaternion movement = (Quaternion.Euler(0, 30 * Mathf.Sin(percent * Mathf.PI), 0));
+                transform.rotation = baseRotation * movement;
+                yield return null;
+            }
+            transform.rotation = baseRotation;
+        }
+        _currentCoroutine = null;
+    }
 
 
     void ComputeCurrentEmotion()
@@ -274,13 +284,37 @@ public class ManagerFaceAnimation : MonoBehaviour
         {
             if (feelingValue > 0)
             {
-                _currentCoroutine = StartCoroutine(NodCoroutine(true, _nextFeeling));
+                _currentCoroutine = StartCoroutine(NodCoroutine(_nextFeeling));
             }
             else
             {
-                _currentCoroutine = StartCoroutine(NodCoroutine(false, _nextFeeling));
+                _currentCoroutine = StartCoroutine(DenyCoroutine( _nextFeeling));
+
+                if (_currentEmotion == ManagerEmotions.Livid && _mouthCoroutine == null)
+                {
+                    _mouthCoroutine = StartCoroutine(MouthCoroutine());
+                }
             }
         }
+    }
+
+    private IEnumerator MouthCoroutine()
+    {
+        int[] tab = {3,2};
+        int index = 0;
+        changeEyes(eyesAnimations[2].name);
+        float time = 0;
+        float frameTime = 0.125f;
+        while (time < 2)
+        {
+            time += frameTime;
+            int i = tab[index];
+            index++;
+            index%= tab.Length;
+            changeMouth(mouthAnimations[i].name);
+            yield return new WaitForSeconds(frameTime);
+        }
+        _mouthCoroutine = null;
     }
 
     void ResetManagerAnimations()
@@ -293,7 +327,7 @@ public class ManagerFaceAnimation : MonoBehaviour
 
     void CargoLeavingHandler(bool isFilled)
     {
-        float feelingValue = (isFilled) ? _cargoValue : -_cargoValue;
+        float feelingValue = (isFilled) ? _cargoCompleteValue : -_cargoEmptyValue;
         AddFeeling(feelingValue);
     }
 
